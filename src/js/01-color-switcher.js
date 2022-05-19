2bb3df9436499363c27e0c83af6f2e1623ab8ac0
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-05-19 07:47:45 +0200

    task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyColor: document.querySelector('body'),
+}
+
+refs.startBtn.addEventListener('click', onStartBtn);
+ 
+refs.stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    refs.startBtn.classList.remove("disabled");
+  });
+
+function onStartBtn() {
+      if(refs.startBtn.classList.contains("disabled")) {
+           return
+           }
+           refs.startBtn.classList.add("disabled");
+           timerId = setInterval(() => {
+                console.log('+')
+                refs.bodyColor.style.backgroundColor = getRandomHexColor();
+            }, 1000);
+            return timerId;
+        }
+
